6c2135b189f01caf110fe73ebe10b9b50ba0a0f9
saadibe <saadibechiring@gmail.com>
2026-06-08 17:53:34 +0200

    feat: lazy routes

---
+++ angular-app/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { roleGuard } from './guards/role.guard';
+
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
+  {
+    path: '', canActivate: [authGuard],
+    loadComponent: () => import('./components/shell/shell.component').then(m => m.ShellComponent),
+    children: [
+      { path: '', redirectTo: 'commandes', pathMatch: 'full' },
+      { path: 'commandes', data: { page: 'commandes' }, canActivate: [roleGuard], loadComponent: () => import('./pages/commandes/commandes.component').then(m => m.CommandesComponent) },
+      { path: 'cuisine',   data: { page: 'cuisine'   }, canActivate: [roleGuard], loadComponent: () => import('./pages/cuisine/cuisine.component').then(m => m.CuisineComponent) },
+      { path: 'tables',    data: { page: 'tables'    }, canActivate: [roleGuard], loadComponent: () => import('./pages/tables/tables.component').then(m => m.TablesComponent) },
+      { path: 'historique',data: { page: 'historique'}, canActivate: [roleGuard], loadComponent: () => import('./pages/historique/historique.component').then(m => m.HistoriqueComponent) },
+      { path: 'admin',     data: { page: 'admin'     }, canActivate: [roleGuard], loadComponent: () => import('./pages/admin/admin.component').then(m => m.AdminComponent) },
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
